3310e3bd36f3ec8e26704a0491b5e9c852f68189
H1012 <1737403035@qq.com>
2020-03-08 15:35:23 +0800

    实时数据API修改

--- src/api/realdata.js
+++ src/api/realdata.js
@@ -1,5 +1,9 @@
 import AXIOS from '@/utils/request'
 // 获取实时数据和综合查询
-export function getRealdata(params) {
+export function ApiGetRealdata(params) {
     return AXIOS.get('/realdata/realdata/', { params })
+  }
+  //获取历史数据
+  export function ApiGetHistorydata(params) {
+    return AXIOS.get('/clientdata/clientdata/', { params })
   }
