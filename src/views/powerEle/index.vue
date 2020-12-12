2c98f11e0e947ca79657a33df1515f15d06076fd
sunshine8866 <2837037335@qq.com>
2020-12-12 20:22:29 +0800

    有功与电流页面bug修改

--- src/views/powerEle/index.vue
+++ src/views/powerEle/index.vue
@@ -23,14 +23,25 @@
               ></el-option>
             </el-select>
           </el-col>
-          <el-col :lg="6" class="col-bg" v-if="datePicker == ''"
-            >起止日期&nbsp;:&nbsp;{{ startdate }}-{{ enddate }}</el-col
-          >
-          <el-col :lg="6" class="col-bg" v-else>时间范围：{{ date }}</el-col>
+          <el-col :lg="5" class="row-bg">
+            时间范围：
+            <el-date-picker
+              style="width:200px"
+              v-model="datePicker"
+              type="daterange"
+              align="right"
+              range-separator="-"
+              start-placeholder="开始时间"
+              end-placeholder="结束时间"
+              size="mini"
+              :picker-options="pickerOptions"
+              value-format="yyyy/MM/dd"
+            ></el-date-picker>
+          </el-col>
           <el-col :lg="3" class="col-bg">
             <el-button
               type="primary"
-              @click="historyLine"
+              @click="submitDate"
               style="margin-top: -10px"
               >时间范围查询</el-button
             >
@@ -40,13 +51,19 @@
     </div>
     <div class="data-chart">
       <el-row :gutter="15" class="elchart">
-        <el-card shadow="never" body-style="padding:0px;">
+        <el-card shadow="never" body-style="padding:0px;" v-loading="loading">
           <div class="chart">
             <LineChart
               ref="power_chart"
               chart-id="power_chart"
               style="height: 350px"
             />
+            <div style="text-align:center;">
+              <el-button icon="el-icon-arrow-left" @click="leftWorkLine" :disabled="leftWorkDis"></el-button>
+              <el-button icon="el-icon-arrow-right" @click="rightWorkLine" :disabled="rightWorkDis"></el-button>
+              <span class="dailylength" style="margin-left:20px;">历史数据个数:{{worklength}}</span>
+              <span class="dailylength" style="margin-left:20px" v-show="currentDiasbled">当前展示第{{this.start_index+1}}、{{this.start_index+2}}、{{this.start_index+3}}组数据</span>
+            </div>
           </div>
         </el-card>
       </el-row>
@@ -59,8 +76,8 @@
             <el-table
               ref="multipleTable"
               stripe
+              :data="tableData.slice((currentPage-1)*pagesize,currentPage*pagesize)" 
               :border="true"
-              :data="tableData"
               class="power-table"
               tooltip-effect="dark"
               style="width: 98%"
@@ -69,8 +86,9 @@
               :row-style="{fontSize:'16px',color:'#212529;',fontWeight:400,'text-align':'center'}"
               @selection-change="handleSelectionChange"
             >
-            <el-table-column type="selection" width="55"> </el-table-column>
-            <el-table-column label="时间">
+            <el-table-column type="selection" width="60" align="center"></el-table-column>
+            <el-table-column type="index" width="60" label="序号" align="center"></el-table-column>
+            <el-table-column label="时间" align="center">
                 <template slot-scope="scope">{{ scope.row.time }}</template>
             </el-table-column>
             <el-table-column prop="name" label="有功">
@@ -79,6 +97,17 @@
               </template>
             </el-table-column>
             </el-table>
+            <div style="margin:15px">
+             <el-pagination
+                @size-change="handleSizeChange"
+                @current-change="handleCurrentChange"
+                :current-page.sync="currentPage"
+                :page-sizes="[10, 20, 30, 50]"
+                :page-size="pagesize"
+                layout="total, sizes, prev, pager, next"
+                :total="powertabletotal">
+              </el-pagination>
+            </div>
         </el-card>
     </div>
     <div class="data-chart">
@@ -120,36 +149,6 @@
         </el-card>
       </el-row>
     </div>
-    <el-dialog title="时间选择" :visible.sync="history" width="400px" top="5vh">
-      <div class="history-middle">
-        <el-row>
-          <el-col :lg="4" :sm="4">时间:</el-col>
-          <el-col :lg="20" :sm="20">
-            <el-date-picker
-              class="datePicker"
-              v-model="datePicker"
-              align="right"
-              type="daterange"
-              range-separator="-"
-              start-placeholder="开始时间"
-              end-placeholder="结束时间"
-              :picker-options="pickerOptions"
-              value-format="yyyy/MM/dd"
-            ></el-date-picker>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :offset="16">
-            <el-button
-              type="primary"
-              style="width: 60px; height: 30px; margin-top: 30px"
-              @click="submitDate"
-              >确定</el-button
-            >
-          </el-col>
-        </el-row>
-      </div>
-    </el-dialog>
   </div>
 </template>
 <script>
@@ -171,34 +170,41 @@ export default {
   },
   data() {
     return {
+        loading: false,
         wellname:"",
         wellid:"",
         wellList:[],
-        startdate:"",
-        enddate:"",
-        datePicker:"",
+        datePicker:[],
         date:"",
+        //左右移动的按钮展示或隐藏
+        leftWorkDis: false,
+        rightWorkDis: true,
+        worklength: [],  // 有功数据长度
+        currentDiasbled:false, // 控制有功当前第几组数据的显示和隐藏
+        // 当前展示有功的第几组数的最小值（比如当前显示第1,2,3组数，start_index=1,end_index=3）
+        start_index: 0,   
+        end_index:0,      
+        power_current_page:1, //有功进行分页
         tableData: [],
+        // 有功接口返回的数据
+        P144Workdata:[],
+        // 表中被选中的数据
         multipleSelection: [],
-        pickerMinDate: '',
+        selectDate: '',
         pickerOptions: {
-          // onPick: ({ maxDate, minDate }) => {
-          //   this.pickerMinDate = minDate.getTime()
-          //   if (maxDate) {
-          //     this.pickerMinDate = ''
-          //   }
-          // },
-          // disabledDate(time) {
-          //   if (this.pickerMinDate !== '') {
-          //     const day30 = (30 - 1) * 24 * 3600 * 1000
-          //     let maxTime = this.pickerMinDate + day30
-          //     if (maxTime > new Date()) {
-          //       maxTime = new Date()
-          //     }
-          //     return time.getTime() > maxTime
-          //   }
-          //   return time.getTime() > Date.now()
-          // },
+          onPick: ({maxDate, minDate}) => {
+            this.selectDate= minDate.getTime();
+            if (maxDate) {
+              this.selectDate= ''
+            }
+          }, disabledDate: (time) => {
+            if (this.selectDate!== '') {
+              const one = 30 * 24 * 3600 * 1000;
+              const minTime = this.selectDate- one;
+              const maxTime = this.selectDate+ one;
+              return time.getTime() < minTime || time.getTime() > maxTime
+            }
+          },
           shortcuts: [
             {
               text: "今天",
@@ -237,7 +243,16 @@ export default {
             }
           ]
         },
-        history:false
+        // 有功接口数据的有功
+        active_list:[],
+        // 有功接口数据的时间
+        time:[],
+        // 表格中的数据总数
+        powertabletotal:0,
+        // 当前页
+        currentPage: 1,
+        // 每页的数据总量
+        pagesize:10
     };
   },
   mounted() {},
@@ -293,7 +308,7 @@ export default {
           return item.id;
         });
         this.wellname = this.wellid[0];
-        this.getPowerChart(this.wellname,this.date);
+        this.getPower(this.wellname,this.date);
         this.getOutputChart(this.wellname,this.date);
         this.getEleHistory(this.wellname,this.date);
         this.powercompare()
@@ -301,79 +316,96 @@ export default {
     },
     // 查看井详情
     getWellDetails(id){
-      this.getPowerChart(id,this.date);
+      this.getPower(id,this.date);
       this.getOutputChart(id,this.date)
       this.getEleHistory(id,this.date)
     },
-    // 选择日期范围
-    historyLine(){
-      this.history = true
-    },
     // 时间选择器点确认
     submitDate() {
+      this.loading = true;
       this.date = this.datePicker[0] + "-" + this.datePicker[1];
-      this.getPowerChart(this.wellname,this.date);
+      this.getPower(this.wellname,this.date);
       this.getOutputChart(this.wellname,this.date);
       this.getEleHistory(this.wellname,this.date);
-      this.history = false;
     },
-    // 获取有功曲线图
-    getPowerChart(id,daterange){
-      // 横轴
-      let x_list = [];
-      // 纵轴  
-      let active_list = [];
-      // 时间
-      let time = [];
-      //s
-      let series = [];
-      for (let i = 1; i <= 145; i++) {
-        x_list.push(i);
-      }
+    handleSizeChange(val) {
+      this.pagesize = val
+    },
+    handleCurrentChange(val) {
+      this.currentPage = val;
+    },
+    // 初始化有功
+    getPower(id,daterange){
       ApiPowerData({id:id,daterange:daterange}).then(({data}) =>{
         this.tableData = data.msg
-        series = data.msg.map(item =>{
-          return{
-            name: item.time,
-            showSymbol: true, // 是否显示点
-            smooth: true, //光滑
-            data: item.p144_data,
-            type: "line"
-          }
-        })
-        let customOption = {
+        this.powertabletotal = this.tableData.length
+        this.worklength = data.msg.length
+        if(this.worklength==0)
+        {
+          this.currentDiasbled=false;//当有功历史数据为0时，隐藏有功的当前第几组数据
+        }
+        else
+        {
+          this.currentDiasbled=true;//当有功历史数据不为0时，显示有功的当前第几组数据
+        }
+        if(this.power_current_page === 1){
+           this.P144Workdata = data.msg;
+           let width = 3;
+           this.start_index = 0;
+           this.end_index = this.start_index + width;
+        }
+        if(this.power_current_page > 1){
+          this.P144Workdata = [...this.P144Workdata,...data.msg];
+        }
+        let array_datas = [];
+        for (let i = this.start_index; i < this.end_index; i++) {
+          array_datas.push(this.P144Workdata[i]);
+        }
+        if (array_datas[2] == undefined) {
+          array_datas.pop();
+        }
+        if (array_datas[1] == undefined) {
+          array_datas.pop();
+        }
+        if (array_datas[0] !== undefined) {
+          array_datas.forEach(item => {
+            this.active_list = this.active_list.concat(item.p144_data);
+            this.time = this.time.concat(item.time)
+          });
+        }
+        this.getPowerChart(this.active_list,this.time)
+        if (this.worklength > 3) {
+          this.leftWorkDis = false;
+          this.rightWorkDis = true;
+        } else if (this.worklength <= 0) {
+          this.leftWorkDis = true;
+          this.rightWorkDis = true;
+        }
+      })
+    },
+    // 获取有功曲线图
+    getPowerChart(active_list,time){
+      // 整个横轴坐标值
+      let x_lists = []
+      // 横轴(第一组145点)
+      let x_list1 = [];
+      // 横轴(第二组145点)
+      let x_list2 = [];
+      // 横轴(第三组145点)
+      let x_list3 = [];
+      let times = 1;
+      for (let i = 1; i <= 145; i++) {
+        x_list1.push(i);
+        x_list2.push(i);
+      }
+      for(let i = 1; i <= 154; i++){
+        x_list3.push(i)
+      }
+      x_lists = x_list1.concat(x_list2).concat(x_list3);
+      let customOption = {
           title: {
             text: " 有功kw",
             left: "center"
-          },
-          tooltip: {
-            trigger: "axis",
-            position: function(point, params, dom, rect, size) {
-              //其中point为当前鼠标的位置，size中有两个属性：viewSize和contentSize
-              var x = point[0]; //
-              var y = point[1];
-              var viewWidth = size.viewSize[0];
-              var viewHeight = size.viewSize[1];
-              var boxWidth = size.contentSize[0];
-              var boxHeight = size.contentSize[1];
-              var posX = 0; //x坐标位置
-              var posY = 0; //y坐标位置
-              if (x < boxWidth) {
-                //左边放不开
-                posX = x + 10;
-              } else {
-                //左边放的下
-                posX = x - boxWidth;
-              }
-              if (y < boxHeight) {
-                //上边放不开
-                posY = 0;
-              } else {
-                //上边放得下
-                posY = y - boxHeight;
-              }
-              return [posX, posY];
-            }
           },
           grid: [
             {
@@ -384,42 +416,46 @@ export default {
           ],
           tooltip: {
             trigger: "axis",
-            position: function(point, params, dom, rect, size) {
-              //其中point为当前鼠标的位置，size中有两个属性：viewSize和contentSize
-              var x = point[0]; //
-              var y = point[1];
-              var viewWidth = size.viewSize[0];
-              var viewHeight = size.viewSize[1];
-              var boxWidth = size.contentSize[0];
-              var boxHeight = size.contentSize[1];
-              var posX = 0; //x坐标位置
-              var posY = 0; //y坐标位置
-              if (x < boxWidth) {
-                //左边放不开
-                posX = x + 10;
-              } else {
-                //左边放的下
-                posX = x - boxWidth;
+            formatter: function(params) {
+              var tip = "";
+              let marker = params[0].marker; 
+              tip = params[0].axisValue+'<br />';
+              if(params[0].dataIndex >=0 && params[0].dataIndex <= 144){
+                tip = tip + marker +'时间：'+ time[0] + '</br>'
+                    + marker + '有功数：'+ params[0].value;
               }
-              if (y < boxHeight) {
-                //上边放不开
-                posY = 5;
-              } else {
-                //上边放得下
-                posY = y;
+              else if (params[0].dataIndex >=145 && params[0].dataIndex <= 289){
+                tip = tip + marker +'时间：'+ time[1] + '</br>'
+                    + marker + '有功数：'+ params[0].value;
+              }else{
+                tip = tip + marker +'时间：'+ time[2] + '</br>'
+                    + marker + '有功数：'+ params[0].value;
               }
-              return [posX, posY];
+              return tip;
+            },
+            axisPointer: {
+              animation: true
             }
           },
           xAxis: {
             type: "category",
-            data: x_list,
+            data: x_lists,
             name: "点数",
             nameTextStyle: { fontSize: 16 },
             boundaryGap: false,
             axisLabel: {
-              interval: 5,
-              fontSize: 13
+              interval: 0,
+              fontSize: 13,
+              formatter: function(value, index) {
+              if (times == 1 && (value == 1 || value == "1")) {
+                return 1;
+              }
+              times += 1;
+              var intvalue = parseInt(value);
+                if (value!=1&&value % 12 == 1) {
+                  return value;
+                }
+              }
             }
           },
           yAxis: {
@@ -427,12 +463,108 @@ export default {
             name: "千瓦",
             nameTextStyle: { fontSize: 16 }
           },
-          series: series
+          series: [{
+            showSymbol: false, // 是否显示点
+            smooth: true, //光滑
+            data: active_list,
+            type: "line",
+            markLine: {
+              symbol: "none", //去掉警戒线最后面的箭头
+              silent: true, //鼠标悬停事件  true没有，false有
+              data: [
+                {
+                  lineStyle: {
+                    //警戒线的样式  ，虚实  颜色
+                    type: "solid",
+                    color: "graylight"
+                  },
+                  xAxis: 144
+                },
+                {
+                  lineStyle: {
+                    //警戒线的样式  ，虚实  颜色
+                    type: "solid",
+                    color: "graylight"
+                  },
+                  xAxis: 289
+                },
+                {
+                  lineStyle: {
+                    //警戒线的样式  ，虚实  颜色
+                    type: "solid",
+                    color: "graylight"
+                  },
+                  xAxis: 434
+                }
+              ]
+            }
+          }]
         };
         this.$nextTick(() => {
+          this.loading = false;
           this.$refs["power_chart"].initChart(customOption);
         })
-      })
+    },
+     //有功左移
+    leftWorkLine() {
+      this.loading = true;
+      this.active_list = [];
+      let width = 3;
+      this.start_index += 1;
+      this.end_index = this.start_index + width;
+      if (this.end_index % 20 === 0){
+        this.power_current_page += 1;
+        this.getPower();
+      }
+      let array_datas = [];
+      for (let i = this.start_index; i < this.end_index; i++) {
+        array_datas.push(this.P144Workdata[i]);
+      }
+      if (array_datas[0] !== undefined) {
+        array_datas.forEach(item => {
+          this.active_list = this.active_list.concat(item.p144_data);
+          this.time = this.time.concat(item.time)
+          this.getPowerChart(this.active_list,this.time);
+        });
+      }
+      if (this.end_index <= this.worklength) {
+        this.rightWorkDis = false;
+        if (this.end_index == this.worklength) {
+          this.leftWorkDis = true;
+        }
+      } else {
+        this.leftWorkDis = true;
+        this.rightWorkDis = false;
+      }
+    },
+    //有功右移
+    rightWorkLine() {
+      this.loading = true;
+      this.active_list = [];
+      let width = 3;
+      this.start_index -= 1;
+      this.end_index = this.start_index + width;
+      let array_datas = [];
+      for (let i = this.start_index; i < this.end_index; i++) {
+        array_datas.push(this.P144Workdata[i]);
+      }
+      if (array_datas[0] !== undefined) {
+        array_datas.forEach(item => {
+          this.active_list = this.active_list.concat(item.p144_data);
+          this.time = this.time.concat(item.time)
+          this.getPowerChart(this.active_list,this.time);
+        });
+      }
+      if (this.start_index >= 0) {
+        // this.getWorkChart();
+        this.leftWorkDis = false;
+        if (this.start_index == 0) {
+          this.rightWorkDis = true;
+        }
+      } else {
+        this.rightWorkDis = true;
+        this.leftWorkDis = false;
+      }
     },
     // 堆叠对比
     powercompare(){
@@ -770,10 +902,12 @@ export default {
   created() {
     var date = new Date();
     var list = this.getDateRange(date,7,true)
-    this.startdate = this.timeFormat(list[0])
-    this.enddate = this.timeFormat(list[1])
-    this.date = this.startdate + "-" + this.enddate
+    console.log(this.datePicker)
+    this.datePicker[0] = this.timeFormat(list[0])
+    this.datePicker[1] = this.timeFormat(list[1])
+    this.date = this.datePicker[0] + "-" + this.datePicker[1]
     this.GetwellList();
+    this.loading = true;
   },
 };
 </script>
